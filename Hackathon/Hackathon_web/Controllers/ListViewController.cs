fbf99f9e4d52ddaad6da65b1918c645bea36c180
ThisIsMyWay <skaczacymuchomor@gmail.com>
2017-06-09 14:05:10 +0200

    initial commit of list

---
+++ Hackathon/Hackathon_web/Controllers/ListViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Hackathon_web.Controllers
+{
+    public class ListViewController : Controller
+    {
+        // GET: ListView
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult HotelList() {
+
+            List<Models.HotelItem> hotelList = new List<Models.HotelItem>
+            {
+                new Models.HotelItem() { Name = "Gotyk House", Address = "Barowa 4" },
+                new Models.HotelItem() { Name = "Some House", Address = "Piwna 4", }
+
+            };
+            return View("HotelListView",hotelList);
+
+        }
+    }
+}
---
+++ Hackathon/Hackathon_web/Models/HotelItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon_web.Models
+{
+
+    public enum DisabilityType
+    {
+          
+    }
+
+    public enum Rate {
+        Poor,
+        Fair, 
+        Good,
+        Very_Good,
+        Excellent
+    }
+
+    public enum FacilitiesType
+    {
+    }
+
+
+
+    public class HotelItem
+    {
+        public Dictionary<DisabilityType, Rate> DisabilityAndRateMap { get; set; }
+        public String Name { get; set; }
+        public String UrlToPicture { get; set; }
+        public Rate HotelRate { get; set; }
+        public Double Price { get; set; }
+        public String Address { get; set; }
+        public Dictionary<FacilitiesType, String> facilitiesAndIcon { get; set; }
+
+    }
+
+    
+}
